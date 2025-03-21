a8d3b9802e597229ad6bd0661f54187f44fbd774
Zahidan Ardhiansyah <zahidan23@gmail.com>
2025-03-21 23:25:08 +0700

    Update : adding user state with redux toolkit

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,13 @@
+import { useSelector } from "react-redux";
+
 function Username() {
-  return <div className="text-semibold hidden text-sm md:block">Zahidan</div>;
+  const username = useSelector((state) => state.user.username);
+
+  if (!username) return null;
+
+  return (
+    <div className="text-semibold hidden text-sm md:block">{username}</div>
+  );
 }
 
 export default Username;
